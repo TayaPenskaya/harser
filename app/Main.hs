0fadfe24333b857ed8dca4e72cdbe57a8d4403ce
tpenskaya <tayaserf@gmail.com>
2020-10-25 23:20:00 +0300

    Upd Main

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,10 @@ module Main where
 import Lexer(Token(..), alexScanTokens)
 import Parser(parseCPP)
 import Grammar(Program(..))
+import CDSL(Interpret(..), test)
 
 main :: IO()
-main = do
-  s <-  getLine
-  print (parseCPP . alexScanTokens $ s) 
+-- main = do
+--   s <-  getLine
+--   print (parseCPP . alexScanTokens $ s) 
+main = print (interpret test)
