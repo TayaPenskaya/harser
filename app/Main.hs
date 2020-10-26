1b68203310cde46862b13ca13a9b32a6fb418280
tpenskaya <tayaserf@gmail.com>
2020-10-26 05:01:37 +0300

    Upd Main

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import Lexer(Token(..), alexScanTokens)
 import Parser(parseCPP)
 import Grammar(Program(..))
-import CDSL(test2, test)
+import CDSL(test2, test, test3, test4)
 
 main :: IO()
 -- main = do
@@ -12,6 +12,10 @@ main :: IO()
 main = do
   res <- test
   res2 <- test2
+  res3 <- test3
+  res4 <- test4
   print(res)
   print(res2)
+  print(res3)
+  print(res4)
   pure ()
