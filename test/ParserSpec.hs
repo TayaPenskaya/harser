d0c32bbb1b4c4a763f903f1ac4cb67d3ea701ca3
tpenskaya <tayaserf@gmail.com>
2020-10-28 22:57:26 +0300

    Upd tests

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -1,4 +1,4 @@
-module ParserTestSpec where
+module ParserSpec where
 
 import Test.Hspec
 import TestUtil
@@ -27,32 +27,32 @@ testParseFoo0WithoutStmt =
   uTest
     "parsing function without stmts"
     (parseCPP . alexScanTokens $ "int main() {}") $
-    Program [Fun0 ValType "main" []]
+    Program [Fun0 FIntType "main" []]
 
 testParseFoo1WithStmt :: SpecWith (Arg Expectation)
 testParseFoo1WithStmt =
    uTest
     "parsing function1 with stmts"
     (parseCPP . alexScanTokens $ "int a(int b) { return b; }") $
-    Program [Fun1 ValType "a" IntType "b" [ReturnStmt (ExprVar "b")]]
+    Program [Fun1 FIntType "a" IntType "b" [ReturnStmt (ExprVar "b")]]
 
 testParseFoo2WithFoo :: SpecWith (Arg Expectation)
 testParseFoo2WithFoo =
    uTest
     "parsing function2 with foo without namespace"
     (parseCPP . alexScanTokens $ "int a(int b, string c) { return d(); }") $
-    Program [Fun2 ValType "a" IntType "b" StringType "c" [ReturnStmt (ExprFun0 ("","d"))]]
+    Program [Fun2 FIntType "a" IntType "b" StringType "c" [ReturnStmt (ExprFun0 ("","d"))]]
 
 testParseFoo2WithStdFoo :: SpecWith (Arg Expectation)
 testParseFoo2WithStdFoo =
    uTest
     "parsing function2 with foo with namespace"
     (parseCPP . alexScanTokens $ "int a(int b, string c) { return std::d(); }") $
-    Program [Fun2 ValType "a" IntType "b" StringType "c" [ReturnStmt (ExprFun0 ("std","d"))]]  
+    Program [Fun2 FIntType "a" IntType "b" StringType "c" [ReturnStmt (ExprFun0 ("std","d"))]]  
 
 testParseFooWithStmts :: SpecWith (Arg Expectation)
 testParseFooWithStmts =
    uTest
     "parsing function with stmts"
     (parseCPP . alexScanTokens $ "int main() {int a = 5; b = 6; return a+b; }") $
-    Program [Fun0 ValType "main" [AssignStmt IntType "a" (ExprVal (IntValue 5)),AssignStmtWithoutType "b" (ExprVal (IntValue 6)),ReturnStmt (ExprPlus (ExprVar "a") (ExprVar "b"))]]
+    Program [Fun0 FIntType "main" [AssignStmt IntType "a" (ExprVal (IntValue 5)),AssignStmtWithoutType "b" (ExprVal (IntValue 6)),ReturnStmt (ExprPlus (ExprVar "a") (ExprVar "b"))]]
