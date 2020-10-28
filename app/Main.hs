019784493c0379ba258c6e6a70ff34141f1f981f
tpenskaya <tayaserf@gmail.com>
2020-10-28 23:00:52 +0300

    Upd Main

--- app/Main.hs
+++ app/Main.hs
@@ -2,20 +2,28 @@ module Main where
 
 import Lexer(Token(..), alexScanTokens)
 import Parser(parseCPP)
-import Grammar(Program(..))
-import CDSL(test2, test, test3, test4)
+import Grammar(Program(..), FunType(..), ValType(..))
+import System.IO
 
 main :: IO()
--- main = do
---   s <-  getLine
---   print (parseCPP . alexScanTokens $ s) 
 main = do
-  res <- test
-  res2 <- test2
-  res3 <- test3
-  res4 <- test4
-  print(res)
-  print(res2)
-  print(res3)
-  print(res4)
-  pure ()
+  s <-  getLine
+  print (parseCPP . alexScanTokens $ s) 
+  
+-- main = do
+  -- res <- test
+  -- res2 <- test2
+  -- res3 <- test3
+  -- res4 <- test4
+  -- print res
+  -- print res2
+  -- print res3
+  -- print res4
+  -- pure ()
+
+-- main = print (parseCPP . alexScanTokens $ "int p(int a, int b) {return (a + b);}")
+
+-- main = do
+--   contents <- readFile "test.txt"
+--   let parsed_contents = parseCPP . alexScanTokens $ contents
+--   print parsed_contents
