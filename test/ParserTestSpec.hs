a939f02f94e46ef4c2f2f37493c814c1a13da261
tpenskaya <tayaserf@gmail.com>
2020-10-28 19:41:35 +0300

    Upd tests

--- test/ParserTestSpec.hs
+++ test/ParserTestSpec.hs
@@ -49,3 +49,10 @@ testParseFoo2WithStdFoo =
     "parsing function2 with foo with namespace"
     (parseCPP . alexScanTokens $ "int a(int b, string c) { return std::d(); }") $
     Program [Fun2 ValType "a" IntType "b" StringType "c" [ReturnStmt (ExprFun0 ("std","d"))]]  
+
+testParseFooWithStmts :: SpecWith (Arg Expectation)
+testParseFooWithStmts =
+   uTest
+    "parsing function with stmts"
+    (parseCPP . alexScanTokens $ "int main() {int a = 5; b = 6; return a+b; }") $
+    Program [Fun0 ValType "main" [AssignStmt IntType "a" (ExprVal (IntValue 5)),AssignStmtWithoutType "b" (ExprVal (IntValue 6)),ReturnStmt (ExprPlus (ExprVar "a") (ExprVar "b"))]]
