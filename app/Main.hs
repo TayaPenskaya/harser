4d446ba6f72b2c647f2a97970576a936284396c3
tpenskaya <tayaserf@gmail.com>
2020-10-31 00:23:45 +0300

    Added cli interface

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,62 @@
 module Main where
 
-import Lexer(Token(..), alexScanTokens)
-import Parser(parseCPP)
-import Grammar(Program(..), FunType(..), ValType(..))
+import Lexer (Token(..), alexScanTokens)
+import Parser (parseCPP)
+import Grammar (Program(..))
 import Translator (translateProgram)
 import Interpreter (interpretCDSLWithST)
+import CDSL (CVar(..))
+
+import Options.Applicative
+import Data.Semigroup ((<>))
+import GHC.Base (returnIO)
+import Control.Monad.IO.Class (liftIO)
+
+data Command = IF String | IS String | PrintS String | PrintF String
+
+interpretString :: Parser Command
+interpretString = IS <$> argument str (metavar "STRING")
+
+interpretFile :: Parser Command
+interpretFile = IF <$> argument str (metavar "FILE")
+
+pprintString :: Parser Command
+pprintString = PrintS <$> argument str (metavar "STRING")
+
+pprintFile :: Parser Command
+pprintFile = PrintF <$> argument str (metavar "FILE")
+
+parser :: Parser Command
+parser = subparser
+        (  command "iS" (info interpretString (progDesc "Interpret code taken from input string"))
+        <> command "iF" (info interpretFile (progDesc "Interpret code taken from file"))
+        <> command "printS" (info pprintString (progDesc "Print code taken from input string"))
+        <> command "printF" (info pprintFile (progDesc "Print code taken from file"))
+        )
+
+
+runInterpret program =  translateProgram $ parseCPP $ alexScanTokens program
+
+run :: Command -> IO ()
+run (IS str) = do
+  res <- runInterpret str
+  print res
+run (IF file) = do
+  ctx <- readFile file
+  res <- runInterpret ctx
+  print res
+run (PrintS str) = print str
+run (PrintF file) = do
+  ctx <- readFile file
+  print ctx
+
+opts :: ParserInfo Command
+opts = info (parser <**> helper)
+            ( fullDesc
+            <> progDesc "Enter command..." 
+            <> header "Program for pretty-printing and interpreting CPP code." )
 
 main :: IO()
--- main = pure ()
-  
--- main = do
-  -- res <- test
-  -- res2 <- test2
-  -- res3 <- test3
-  -- res4 <- test4
-  -- print res
-  -- print res2
-  -- print res3
-  -- print res4
-  -- pure ()
-
--- main = print (parseCPP . alexScanTokens $ "int p(int a, int b) { std::cin >> 1+1; }")
-
--- main = do
---   contents <- readFile "test.txt"
---   let parsed_contents = parseCPP . alexScanTokens $ contents
---   print parsed_contents
-
 main = do
-  line <- getLine
-  print (interpretCDSLWithST $ translateProgram $ parseCPP $ alexScanTokens line)
+  args <- execParser opts 
+  run args
